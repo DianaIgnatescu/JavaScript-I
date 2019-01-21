0be640cc813f8c767deba81b630e450a5777f755
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-01-21 14:18:10 +0000

    STRETCH: Completed objects stretch and fixed challenge 3 in arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -71,7 +71,7 @@ function findCar(inventory, key, value) {
       return inventory[i];
     }
   }
-  return null;
+  // return null;
 }
 
 findCar(inventory, 'id', 33);
@@ -93,7 +93,7 @@ console.log(`The last car in the inventory is a ${inventory[49].car_make} ${inve
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 for (let i = 0; i < inventory.length; i++) {
-  carModels.push(inventory[i].car_model);
+  carModels.push(inventory[i].car_model.toLowerCase());
 }
 console.log(carModels.sort());
 
